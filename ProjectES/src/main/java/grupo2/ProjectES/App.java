0a16afcfde2c46bfa9f84695e31535a9e0ea3b4b
Dacba28 <dacba@iscte-iul.pt>
2022-10-27 15:38:26 +0100

    Teste De Funcionamento

--- ProjectES/src/main/java/grupo2/ProjectES/App.java
+++ ProjectES/src/main/java/grupo2/ProjectES/App.java
@@ -4,10 +4,11 @@ package grupo2.ProjectES;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+   
+	public static void main( String[] args ){
+    
+		System.out.println( "Hello World! E Sou LINDOOOOOOOOOOOO!!!!!!!!!" );
+    
+	}
 }
