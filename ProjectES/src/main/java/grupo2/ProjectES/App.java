0a2c38069e60fa3cb888b99c18feeb3a1feaf77e
tfava1 <tfava1@iscte-iul.pt>
2022-10-27 15:12:14 +0100

    Push do projeto do pc do Tiago para o repositório

---
+++ ProjectES/src/main/java/grupo2/ProjectES/App.java
@@ -0,0 +1,13 @@
+package grupo2.ProjectES;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
