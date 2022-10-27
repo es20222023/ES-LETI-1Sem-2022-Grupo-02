4811e7b5c5dc6fd5a632c59258dea0e0607a2dea
tfava1 <tfava1@iscte-iul.pt>
2022-10-27 15:52:48 +0100

    Teste de commit utilizando o Eclipse

--- ProjectES/src/main/java/grupo2/ProjectES/App.java
+++ ProjectES/src/main/java/grupo2/ProjectES/App.java
@@ -8,7 +8,7 @@ public class App {
    
 	public static void main( String[] args ){
     
-		System.out.println( "Hello World! E Sou LINDOOOOOOOOOOOO!!!!!!!!!" );
+		System.out.println( "Hello World! E Sou LINDO!" );
     
 	}
 }
