e1878d55dfd2739bdc977d39c920be54b2379e97
jamrs1 <jamrs1@iscte-iul.pt>
2022-10-31 18:25:09 +0000

    testejoana

--- ProjectES/src/main/java/grupo2/ProjectES/App.java
+++ ProjectES/src/main/java/grupo2/ProjectES/App.java
@@ -9,6 +9,7 @@ public class App {
 	public static void main( String[] args ){
     
 		System.out.println( "Hello World! Eu sou LINDO!" );
-    
+		System.out.println("teste teste 1 2 3 sommmm, som som 1 2 3");
+		System.out.println("eu sou maisss");
 	}
 }
