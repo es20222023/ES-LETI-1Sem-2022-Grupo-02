a2d028a0a1b0d8984ec8929c00e6e7a85a6f06cb
tfava1 <tfava1@iscte-iul.pt>
2022-11-03 00:40:01 +0000

    Alteração do FILE_NAME da classe HttpGetHorarioFile

--- ProjectES/src/main/java/grupo2/ProjectES/HttpGetHorarioFile.java
+++ ProjectES/src/main/java/grupo2/ProjectES/HttpGetHorarioFile.java
@@ -9,7 +9,7 @@ public class HttpGetHorarioFile {
 	public static void main(String[] args) {
 		
 		String FILE_URL="https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=tfava1@iscte.pt&password=hRocBUdgWhnQU3ua4S6EqOSz34MyzMSHGxAGsRmQsDdmi6rHEn37kBhMswleKA4yQm8D9twF0406T0fjzF52uSiPLChjRZ0FjTcMZgYAmdNearUjE87RX0DmZbhgDqfr";
-		String FILE_NAME="C:\\Users\\Tiago Vieira\\Desktop\\file.txt"; // DUVIDA
+		String FILE_NAME="src\\main\\java\\Files\\file.txt";
 		try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
 				  FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
 				    byte dataBuffer[] = new byte[1024];
